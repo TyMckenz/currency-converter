0caf574c3d5d36002b05fb94496a73b0b30bbaae
Tyler M <t_mckenzie@outlook.com>
2022-09-21 12:07:41 -0500

    First steps in assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ConverterComponent } from './converter/converter.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ConverterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
